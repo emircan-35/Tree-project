cba3cfe92b64a6c67d6b8148f38a2fef05edf049
Emircan Tepe <emircan.tepe@ogr.deu.edu.tr>
2021-08-17 20:34:55 +0300

    added some basic of program class

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,31 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import enigma.core.Enigma;
 
 public class Program {
+	private Tree tree;
 	private enigma.console.Console cn;
-	
+	private KeyListener klis;
+	private int keypr;
+	private int rkey;
+	private int rkeymod;
 	Program(){
 		this.cn = Enigma.getConsole("Tree App", 120, 30, 15);
+		startProgram();
 	}
+	public void startProgram() {
+		klis=new KeyListener() {
+			public void keyTyped(KeyEvent e) {}
+			public void keyPressed(KeyEvent e) {
+				if(keypr==0) {
+					keypr=1;
+					rkey=e.getKeyCode();
+					rkeymod=e.getModifiersEx();}}public void keyReleased(KeyEvent e) {
+					}
+					};
+		cn.getTextWindow().addKeyListener(klis);		
+	}
+    
 }
+
